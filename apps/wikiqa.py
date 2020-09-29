6b4c3df88dc1bd6674de6def77c6a906dfe617ae
Melanie Beck <melaniebeck81@gmail.com>
2020-09-29 18:05:30 -0500

    lots of work on wikiqa app

--- apps/wikiqa.py
+++ apps/wikiqa.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import wikipedia as wiki
-from transformers import pipeline
-
+import pandas as pd
+from PIL import Image
+import requests
+from io import BytesIO
+from transformers import (
+    pipeline, 
+    AutoModelForQuestionAnswering,
+    AutoTokenizer
+)
 
 MODEL_OPTIONS = {
     "BERT":         "deepset/bert-base-cased-squad2",
@@ -13,41 +20,105 @@ MODEL_OPTIONS = {
 
 @st.cache(allow_output_mutation=True)
 def load_model(model_choice):
-    model = MODEL_OPTIONS[model_choice]
-    return pipeline('question-answering', model=model, tokenizer=model)
+    model_name = MODEL_OPTIONS[model_choice]
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = pipeline('question-answering', model=model_name, tokenizer=tokenizer)
+    return model
 
+def highlight_text(segment, context, full_text=False):
+    if segment not in context:
+        return 
+    
+    length = len(segment)
 
+    if full_text: 
+        # find the section the answer was found in and display only that section
+        chunks = context.split("==") 
+        for chunk in chunks:
+            if segment in chunk:
+                idx = chunk.index(segment)
+                chunk1 = chunk[:idx]
+                chunk2 = chunk[idx:idx+length]
+                chunk3 = chunk[idx+length:]
+                break
+    else:
+        idx = context.index(segment)
+        chunk1 = context[:idx]
+        chunk2 = context[idx:idx+length]
+        chunk3 = context[idx+length:]
 
+    new_context = chunk1 + '<span style="background-color: #FFFF00"> **' + chunk2 + '** </span>' + chunk3
+    return new_context
+
+def make_url(segment, url):
+    new_segment = f'<a target="_blank" href="{url}">{segment}</a>'
+    return new_segment
+
+# ------ SIDEBAR SELECTIONS ------ 
 model_choice = st.sidebar.selectbox(
     "Choose a Transformer model:",
     list(MODEL_OPTIONS.keys())
 )
 
+number_of_pages = st.sidebar.slider(
+    "How many Wikipedia pages should be displayed?", 1, 5, 1)
+
+number_of_answers = st.sidebar.slider(
+    "How many answers should the model suggest for each Wikipedia page?", 1,5,1)
+
+use_full_text = st.sidebar.checkbox(
+    "Use full text rather than summaries of Wikipedia pages (slower but better answers)"
+)
+
+st.title("Question Answering with ")
+response = requests.get("https://upload.wikimedia.org/wikipedia/commons/5/53/Wikipedia-logo-en-big.png")
+st.image(Image.open(BytesIO(response.content)), width=400)
+
+st.markdown("This app demonstrates how to build a simple question answering system on Wikipedia. \
+    You can choose one of five Transformer models that have already been trained for extractive \
+    question answering. When selecting a model, if that model has never been called before, its \
+    weights will be downloaded before it can be used for question answering. For large \
+    models this can take quite some time. Once cached, switching between models will be seemless.")
+
+st.markdown("A question entered below will first be used in Wikipedia's default search engine, \
+    resulting in a ranked list of Wikipedia pages that are most related to the question. \
+    The question and each Wikipedia page are then sent to the QA model, which returns answers \
+    extracted from the text. These answers are displayed below a snippet of the Wikipedia article. \
+    By default, only the page summary is used when searching for answers to questions. \
+    This saves time since many Wikipedia pages are very long and QA models can be very slow. \
+    However you can choose to use the full text of the article in the sidebar. This usually provides \
+    better results, though it does take longer to process.")
+
+# LOAD MODEL
 reader = load_model(model_choice)
 
-st.title("Basic QA with Wikipedia")
+# Get question
+st.markdown("## Ask a question")
+query = st.text_input('Enter text here', 'Why is the sky blue?')
+st.markdown(f"## Displaying the top {number_of_pages} results:")
 
-question = st.text_input('Ask a Question', 'Why is the sky blue?')
+# Wiki search
+results = wiki.search(query, results=number_of_pages)
+for i, result in enumerate(results):
+    wiki_page = wiki.page(result, auto_suggest=False)
 
-results = wiki.search(question)
+    title_url = make_url(result, wiki_page.url)
+    st.markdown("### "+ str(i+1)+') '+title_url, unsafe_allow_html=True)
 
-for result in results:
-  st.markdown(wiki.page(result))
+    if use_full_text:
+        context = wiki_page.content
+    else:
+        context = wiki_page.summary
+    inputs = {'question':query, 'context':context}
+    answers = reader(inputs, **{'topk':number_of_answers})
 
-page = wiki.page(results[0])
-st.write(f"Top result: {page}")
+    try: 
+        answerdf = pd.DataFrame(answers)
+    except: 
+        answerdf = pd.DataFrame(answers, index=[0]) 
 
-topk = st.slider("Return top k answers", 1,5,1)
+    hilite_context = highlight_text(answerdf['answer'][0], context, full_text=use_full_text)
+    st.markdown(hilite_context, unsafe_allow_html=True)
 
-inputs = {'question':question, 'context':page.content}
-answers = reader(inputs, **{'topk':topk})
-
-answerdf = pd.DataFrame(answers)
-st.table(answerdf)
-
-#st.write("Answer:", reader(inputs, **{'topk':topk}))
-
-st.markdown('###### Found from this Wikipedia page:')
-st.markdown(f'<a target="_blank" href="{page.url}">{page.title}</a>',
-            unsafe_allow_html=True)
-
+    answerdf.drop(columns=['start', 'end'], inplace=True)
+    st.table(answerdf)
